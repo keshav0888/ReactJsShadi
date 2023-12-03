f5e4666c01915a986884efdd0d3ae73b20b50253
keshavshinde888 <147683001+keshavshinde888@users.noreply.github.com>
2023-12-03 21:34:26 +0530

    changes

--- shadi/src/App.js
+++ shadi/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Header } from './Components/Header'
 import { Register } from './Pages/Register'
+import { LoginForm } from './Pages/LoginForm'
 export const App = () => {
   return (
     // <Header/>
-    <Register/>
+    <LoginForm/>
   )
 }
---
+++ shadi/src/Pages/LoginForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+export const LoginForm = () => {
+  const onRegisterClick=()=>{
+     return 
+  }
+  return (
+    <div className='loginForm'>
+        <button>Login</button>
+        <button>Register</button>
+    </div>
+  )
+}
